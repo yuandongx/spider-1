850a825402b68a016a0b6e4c066e4f002017b66e
yuandongx <yuandongx@126.com>
2025-05-09 15:53:46 +0800

    update

---
+++ setup/start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+LOGS=logs
+DATA='data'
+CELERY_SCHEDULE_FILE='logs/celery.beat.schedule'
+CELERY_WORKER_LOG='logs/celery.worker.log'
+CELERY_BEAT_LOG='logs/celery.beat.log'
+
+if [ -d "$LOGS" ];then
+    echo 'logs dir is exists.'
+else
+    mkdir $LOGS
+fi
+
+if [ -d "$DATA" ]; then
+    echo 'data dir is exists.'
+else
+    mkdir $DATA
+fi
+echo Start app server...
+
+python3 -m gunicorn -c gunconfig.py main:app
+
+
+echo Celery worker is starting...
+celery -A spider worker --concurrency 3 --loglevel  info --logfile $CELERY_WORKER_LOG
+
+
+echo Celery beat is starting...
+celery -A spider beat --loglevel  info --logfile $CELERY_BEAT_LOG --schedule $CELERY_SCHEDULE_FILE --detach
